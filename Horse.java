bfff4b9ab13955752f1ab5587148d42bde288a26
ChrisMitov <mitov.christian@gmail.com>
2015-12-10 10:53:32 +0200

    Horse path

--- Horse.java
+++ Horse.java
@@ -4,15 +4,15 @@ public class Horse {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Въведете размери на матрицата: ");
+		System.out.print("Р’СЉРІРµРґРµС‚Рµ СЂР°Р·РјРµСЂРё РЅР° РјР°С‚СЂРёС†Р°С‚Р°: ");
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		char[][] arr = new char[n][m];
-		System.out.print("Въведете координати на коня:");
+		System.out.print("Р’СЉРІРµРґРµС‚Рµ РєРѕРѕСЂРґРёРЅР°С‚Рё РЅР° РєРѕРЅСЏ:");
 		int x = sc.nextInt();
 		int y = sc.nextInt();
 		if (x < 0 || x >= arr.length || y < 0 || y >= arr[0].length) {
-			System.out.println("Некоректно въведени стойности за коня.");
+			System.out.println("РќРµРєРѕСЂРµРєС‚РЅРѕ РІСЉРІРµРґРµРЅРё СЃС‚РѕР№РЅРѕСЃС‚Рё Р·Р° РєРѕРЅСЏ.");
 			sc.close();
 			return;
 		}
